1fbcaa167ba25fdf3c28d45710f91374e18d9768
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2014-07-18 18:25:29 +0900

    New m file & mdl file Upload

---
+++ MATLAB/Simulink_SimMechanics files/Plant_Parameter.m
@@ -0,0 +1,22 @@
+%角度を初期値として微分により角速度、角加速度計算
+clear;
+
+%左足パラメータ
+Thigt_L_Pitch = 0;
+Thigt_L_Roll = 0;
+Thigt_L_Yaw = 0;
+
+Knee_L_Pitch = 20;
+
+Ankle_L_Roll = 0;
+Ankle_L_Pitch = 0;
+
+%右足パラメータ
+Thigt_R_Pitch = 0;
+Thigt_R_Roll = 0;
+Thigt_R_Yaw = 0;
+
+Knee_R_Pitch = 0;
+
+Ankle_R_Roll = 0;
+Ankle_R_Pitch = 0;
