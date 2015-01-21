b3615c5edece3fe677d999471bc02a92b6736771
marsryu <marsryu@ubuntu.ubuntu-domain>
2015-01-21 13:07:41 +0900

    Add new file

--- Genetic Algorithm/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ga.h"
-
-int main()
-{
-	ga ob(10);	//オブジェクト作成、初期集団10個体作成
-
-	return 0;
-
-}
---
+++ Genetic Algorithm/pendlum.cpp
@@ -0,0 +1,8 @@
+/*
+ * @file    pendlum.cpp
+ * @brief   Control of Invert Pendlum using Genetic Algorithm
+ * @date    2015.1.21
+ * @author  Ryu Yamamoto
+*/
+#include <pendlum.h>
+
